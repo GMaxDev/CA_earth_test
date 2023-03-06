658efdd110a9ad520e46bd3ac51400e2977838e8
gmaxdev <maxime.guigra@gmail.com>
2023-03-07 00:00:29 +0100

    Finalisation alphabet

--- alphabet.js
+++ alphabet.js
@@ -1,6 +1,9 @@
-let all_letter = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+let all_letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let alphabet = []
 
-for(i=0; i<all_letter.length(); i++)
+for(i=0; i<all_letter.length; i++)
 {
-    console.log(i)
+    alphabet += all_letter[i]
 }
+
+console.log(alphabet)
